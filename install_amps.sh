662a4744ae574d575bc98cf78702ac7b0fafed39
Rob Wilds <robwilds@me.com>
2025-06-27 12:21:21 -0400

    updated yaml and included hyland theme and other minor changes

--- install_amps.sh
+++ install_amps.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 docker cp support-tools-repo-1.2.3.0-SNAPSHOT-amp.amp community251-alfresco-1:/usr/local/tomcat/amps
-
-
 Docker cp support-tools-share-1.2.3.0-SNAPSHOT-amp.amp community251-share-1:/usr/local/tomcat/amps_share
 
 
 docker exec -it --user root community251-alfresco-1 bash -c "java -jar /usr/local/tomcat/alfresco-mmt/alfresco-mmt-25.1.0.71.jar install /usr/local/tomcat/amps /usr/local/tomcat/webapps/alfresco -directory -nobackup -force"
-
 docker exec -it --user root community251-share-1 bash -c "java -jar /usr/local/tomcat/alfresco-mmt/alfresco-mmt-25.1.0.71.jar install /usr/local/tomcat/amps_share /usr/local/tomcat/webapps/share -directory -nobackup -force"
--- install_jars.sh
+++ install_jars.sh
@@ -1,4 +1,6 @@
-#Install jars that are placed in the amp folder
-
-#This installs all jars present to alfresco (content) container
-docker cp ./*platform*.jar community251-alfresco-1:/usr/local/tomcat/webapps/alfresco/WEB-INF/lib
+#!/bin/bash
+echo "This installs all jars present to alfresco (content) container"
+docker cp ./leonardo5-platform-1.0-SNAPSHOT.jar community251-alfresco-1:/usr/local/tomcat/webapps/alfresco/WEB-INF/lib
+echo "This installs all jars present to share container"
+docker cp ./hylandtheme-share-1.0-SNAPSHOT.jar community251-share-1:/usr/local/tomcat/webapps/share/WEB-INF/lib
+echo "done copying jars"
