4dc8e7849d14c1ffdde74a615e26e87ff61a8e25
Rob Wilds <robwilds@me.com>
2025-07-23 11:10:25 -0400

    updated version numbers

--- installs/install_jars.sh
+++ installs/install_jars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "This installs all jars present to alfresco (content) container"
-docker cp ./leonardo5-platform-1.0-SNAPSHOT.jar community251-alfresco-1:/usr/local/tomcat/webapps/alfresco/WEB-INF/lib
+docker cp ./leonardo5-platform-1.0-SNAPSHOT.jar community25x-alfresco-1:/usr/local/tomcat/webapps/alfresco/WEB-INF/lib
 echo "This installs all jars present to share container"
-docker cp ./hylandtheme-share-1.0-SNAPSHOT.jar community251-share-1:/usr/local/tomcat/webapps/share/WEB-INF/lib
+docker cp ./hylandtheme-share-1.0-SNAPSHOT.jar community25x-share-1:/usr/local/tomcat/webapps/share/WEB-INF/lib
 echo "done copying jars"
