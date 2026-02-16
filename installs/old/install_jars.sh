31564daa4dcd6ffe1d40eb192e293174e349e501
Rob Wilds <robwilds@me.com>
2026-02-16 15:06:27 -0500

    created more intelligent install script and cleaned up folder structure

--- installs/old/install_jars.sh
+++ installs/old/install_jars.sh
@@ -6,5 +6,3 @@ docker cp ./alfresco-b64-jscript-root-1.0.0.jar community25x-alfresco-1:/usr/loc
 echo "This installs all jars present to share container"
 docker cp ./hylandtheme-share-1.0-SNAPSHOT.jar community25x-share-1:/usr/local/tomcat/webapps/share/WEB-INF/lib
 echo "done copying jars"
-
-
